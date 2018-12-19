4db5fd1a60d0ef57b1a14f943d104bb7825caa4e
Miklos Nemeth <nemethmiklos1960@gmail.com>
2018-12-19 23:10:11 +0100

    AppBar, Toolbars added with Pixabay link button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Button from "@material-ui/core/Button"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="secondary">
-        Pixabay Image Finder
-        <img src={logo} className="App-logo" alt="logo" width="56px"/>
-      </Button>
+      <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <img src={logo} className="App-logo" alt="logo" width="56px"/>
+          <Typography variant="h6" noWrap color="inherit">Pixabay Image Finder</Typography>
+        </Toolbar>
+        <Toolbar>
+          <a href="https://pixabay.com/">
+            <img src="https://pixabay.com/static/img/public/leaderboard_b.png" alt="Pixabay" width="100%"/>
+          </a>
+        </Toolbar>
+      </AppBar>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit possimus fuga exercitationem illo, quam sunt sint veniam enim nemo molestias, minus consequatur odit, a quo aspernatur! Blanditiis reiciendis doloribus quas.
+      </div>
+      </>
     )
   }
 }
